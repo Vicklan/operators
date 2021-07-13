945bd7c8ff6257404a28d4e010219d13f0d59b90
Vicklan <vicklan132@gmail.com>
2021-07-13 16:24:42 +0300

    Practise

---
+++ operators.py
@@ -0,0 +1,19 @@
+'''
+You are asked to develop ta simple app to calculate the users age based on current year and date of birth
+'''
+currentYear = 2021
+yearOfBirth = 2000
+age = 0
+
+age = currentYear - yearOfBirth
+print(age)
+
+'''
+Given the exclusive price and VAT of milkalculate the inclusive price 
+'''
+exclusivePrice = 45
+vatTax = 16/100
+vatAmount =vatTax*exclusivePrice
+inclusivePrice =exclusivePrice+vatAmount
+print(inclusivePrice)
+
---
+++ scope.py
@@ -0,0 +1,20 @@
+name = "Computer Pride"
+def getName():
+    Name = "Sam"
+    print(name)
+
+getName()
+print(name)
+
+ '''
+Wafula is a manager at Tuskys Supermarket, he has asked you to develop a simple application that will calculate the price of items
+in the supermarket
+ '''
+
+ manager = "Wafula"
+ productName = 55.06
+ inclPrice = "Milk"
+
+
+ 
+ 
